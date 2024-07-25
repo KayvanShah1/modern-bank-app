c91b287ac4432b14856198c5e4f91a0009c56106
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2024-07-24 17:36:51 -0700

    other components

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "../style";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section
+      id="cta"
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} bg-black-gradient-2 box-shadow rounded-[20px]`}
+    >
+      <div>
+        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+          Everything you need to accept card payments and grow your business anywhere in the world.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default CTA;
